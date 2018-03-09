81624a524c726500492832097d3b2ba48d03b92a
橋本翔太 <hasi6363@gmail.com>
2018-03-09 21:42:17 +0900

    Initial Commit

---
+++ SignalGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SignalGenerator
+//
+//  Created by 橋本翔太 on 2018/03/09.
+//  Copyright © 2018 hasi6363. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

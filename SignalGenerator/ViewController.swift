a61c5880262022d63fec2351fc5b45ba669d9551
橋本翔太 <hasi6363@MacBook-Pro.local>
2018-03-23 23:35:53 +0900

    updated ViewController

--- SignalGenerator/ViewController.swift
+++ SignalGenerator/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //var mIsPlaying:Bool = false
+    var sg = SignalGenerator()
+    
+    @IBOutlet weak var mOutlet_Label: UILabel!
+    @IBOutlet weak var mOutlet_Slider: UISlider!
+    @IBOutlet weak var mOutlet_Button: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        //mIsPlaying = false;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func Action_SliderValueChanged(_ sender: UISlider)
+    {
+        sg.mFrequency = 2 * pow(10, sender.value)
+        mOutlet_Label.text = sg.mFrequency.description
+    }
+    @IBAction func Action_ButtonTouchUp(_ sender: UIButton)
+    {
+        
+        if(sg.mIsPlaying)
+        {
+            mOutlet_Button.setTitle("Play", for:UIControlState.normal)
+            //sg.mIsPlaying = false;
+            sg.stop()
+        }
+        else
+        {
+            mOutlet_Button.setTitle("Pause", for:UIControlState.normal);
+            //mIsPlaying = true
+            sg.play()
+        }
+    }
 }
 

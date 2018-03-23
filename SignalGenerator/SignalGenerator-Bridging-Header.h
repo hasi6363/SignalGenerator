79abc35e24b7344682f967628b8247c865230a96
橋本翔太 <hasi6363@MacBook-Pro.local>
2018-03-23 23:30:25 +0900

    created SignalGenerator

---
+++ SignalGenerator/SignalGenerator-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SignalGenerator-Bridge-Header.h
+//  SignalGenerator
+//
+//  Created by 橋本翔太 on 2018/03/21.
+//  Copyright © 2018 hasi6363. All rights reserved.
+//
+
+#ifndef SignalGenerator_Bridge_Header_h
+#define SignalGenerator_Bridge_Header_h
+
+
+#endif /* SignalGenerator_Bridge_Header_h */
---
+++ SignalGenerator/SignalGenerator.m
@@ -0,0 +1,9 @@
+//
+//  SignalGenerator.m
+//  SignalGenerator
+//
+//  Created by 橋本翔太 on 2018/03/15.
+//  Copyright © 2018 hasi6363. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

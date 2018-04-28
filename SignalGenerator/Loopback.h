195366c90d754421b9c591f87e726de9ce329eb7
Hashimoto Shota <hasi6363@gmail.com>
2018-04-29 00:32:43 +0900

    add Loopback.mm

---
+++ SignalGenerator/Loopback.h
@@ -0,0 +1,13 @@
+//
+//  Loopback.h
+//  SignalGenerator
+//
+//  Created by 橋本翔太 on 2018/04/28.
+//  Copyright © 2018 hasi6363. All rights reserved.
+//
+
+#ifndef Loopback_h
+#define Loopback_h
+
+
+#endif /* Loopback_h */
---
+++ SignalGenerator/Loopback.mm
@@ -0,0 +1,9 @@
+//
+//  Loopback.m
+//  SignalGenerator
+//
+//  Created by 橋本翔太 on 2018/04/28.
+//  Copyright © 2018 hasi6363. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
